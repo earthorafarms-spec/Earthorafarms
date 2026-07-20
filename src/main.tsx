be3787f60bb2b9552b9906bd861310f6995721f1
Devarsh Joshi <mr.devarsh004@gmail.com>
2026-07-20 11:32:07 +0530

    fix: remove redundant CartProvider wrap in main.tsx causing render lifecycle error

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import { CartProvider } from './contexts/cart-context';
 import { Toaster } from './components/ui/toaster';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <CartProvider>
+  <>
     <App />
     <Toaster />
-  </CartProvider>
+  </>
 );
